dcbf7080026b538b34084afe542fc259b24e438a
dom7k <dominicnaatey@gmail.com>
2024-06-01 13:16:53 +0000

    Import LangChain Components and Initialize Bedrock Client

--- Admin/admin.py
+++ Admin/admin.py
@@ -1,7 +1,12 @@
 import boto3
+from langchain_community.embeddings import BedrockEmbeddings ## Bedrock
+from langchain.text_splitter import RecursiveCharacterTextSplitter ## Text Splitter
+from langchain_community.document_loaders import PyPDFLoader ## Pdf Loader
+from langchain_community.vectorstores import FAISS ## import FAISS
 import streamlit as st
 import os
 import uuid
+
 
 ## s3_client
 s3_client = boto3.client("s3")
