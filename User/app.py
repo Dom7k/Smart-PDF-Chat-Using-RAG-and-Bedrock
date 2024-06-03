dea144b6f50cfe5c69457f0318658c8b05e7746d
dom7k <dominicnaatey@gmail.com>
2024-06-03 09:51:58 +0000

    Main Function for Streamlit Application

--- User/app.py
+++ User/app.py
@@ -6,3 +6,15 @@ from langchain_community.vectorstores import FAISS ## import FAISS
 import streamlit as st
 import os
 import uuid
+
+   
+# Main Function for Streamlit Application
+## main method
+def main():
+    st.header("This is Client Site for Chat with PDF demo using Bedrock, RAG etc")
+   
+
+
+
+if __name__ == "__main__":
+    main()
