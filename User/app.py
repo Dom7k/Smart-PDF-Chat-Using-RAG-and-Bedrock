178954da6ecf2fa61ae8f55064c81b058820f84f
dom7k <dominicnaatey@gmail.com>
2024-06-03 14:48:43 +0000

    Generate Response Using LLM and FAISS Vector Store

--- User/app.py
+++ User/app.py
@@ -35,6 +35,37 @@ def get_llm():
                   model_kwargs={'max_tokens_to_sample': 512})
     return llm
 
+# get_response()
+def get_response(llm,vectorstore, question ):
+    ## create prompt / template
+    prompt_template = """
+
+    Human: Please use the given context to provide concise answer to the question
+    If you don't know the answer, just say that you don't know, don't try to make up an answer.
+    <context>
+    {context}
+    </context>
+
+    Question: {question}
+
+    Assistant:"""
+
+    PROMPT = PromptTemplate(
+        template=prompt_template, input_variables=["context", "question"]
+    )
+
+    qa = RetrievalQA.from_chain_type(
+    llm=llm,
+    chain_type="stuff",
+    retriever=vectorstore.as_retriever(
+        search_type="similarity", search_kwargs={"k": 5}
+    ),
+    return_source_documents=True,
+    chain_type_kwargs={"prompt": PROMPT}
+)
+    answer=qa({"query":question})
+    return answer['result']
+
 # Main Function for Streamlit Application
 ## main method
 def main():
@@ -42,10 +73,10 @@ def main():
 
     load_index()
 
-    # List files in the folder_path
-    dir_list = os.listdir(folder_path)
-    st.write(f"Files and Directories in {folder_path}")
-    st.write(dir_list)
+    # # List files in the folder_path
+    # dir_list = os.listdir(folder_path)
+    # st.write(f"Files and Directories in {folder_path}")
+    # st.write(dir_list)
 
     ## create index
     faiss_index = FAISS.load_local(
@@ -56,7 +87,7 @@ def main():
     )
 
     st.write("INDEX IS READY")
-    question = st.text_input("Please ask your question")
+    question = st.text_input("what is your question?")
     if st.button("Ask Question"):
         with st.spinner("Querying..."):
 
